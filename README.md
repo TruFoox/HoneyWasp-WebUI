3f7c449426a1be6eaddf2028e24d690a50f6469d
Landen Laflamme <landenlaflamme@gmail.com>
2026-09-08 16:42:31 -0700

    Added basic check for honeywasp client

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ An attempt at a WebUI for [TruFoox/HoneyWasp](https://github.com/TruFoox/HoneyWa
 
 
 Will probably not go well, as I dont know CSS or Javascript before going into it
+
+
+Examples used:
+https://github.com/JAmanOG/WebSocket-Basic-Implementation
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,69 @@
+import { useEffect, useState } from 'react'
 import mainLogo from './assets/Logo.svg'
-
 import './App.css'
 function App() {
+    // Apparently js variables need a setter defined alongside it for it to change dynamically w/ react
+    const [connected, setConnected] = useState(false) // Usestate links the setter and the var itself
+    const webUIVersion = "v1.0" // <Datatype1 | Datatype 2>(initial_state)
+    const [honeyWaspVersion, setHoneyWaspVersion] = useState<string | null>("Not Fetched")
 
-    /* Ignore errors on IntelliJ, boy is it stupid. */
-  return (
-    <>
-    <section id="bottom-left">
-        <div className="logo">
-          <img src={mainLogo} className = "logoImage" alt="HoneyWasp logo" />
-          <span className="repoinfo">TruFoox/HoneyWasp WebUI v1.0</span>
-        </div>
-    </section>
-    <section id="center">
-        <div>
-            <h1>Hi</h1>
-            <p>
-               Hi <code>nerd</code>
-            </p>
-        </div>
-    </section>
-    </>
-  )
+    let socket: WebSocket // equal to WebSocket socket;
+
+    useEffect(() => { // "Run this when something happens"
+        const socket = new WebSocket('ws://localhost:8080')
+
+        socket.onopen = () => {
+            console.log('Connected to HoneyWasp server')
+            socket.send('WebUI ready')
+        }
+
+        socket.onmessage = (event) => {
+            console.log('Server:', event.data)
+
+            if (event.data === 'Acknowledged') {
+                setConnected(true)
+            } else {
+                setHoneyWaspVersion(event.data)
+            }
+        }
+
+        return () => {
+            socket.close()
+        }
+    }, []) // [] means on startup after website renders
+
+    useEffect(() => {
+        if (connected) {
+
+            socket.send(honeyWaspVersion)
+            setHoneyWaspVersion(honeyWaspVersion);
+        }
+    }, [connected]) // "Send once connected is true"
+
+    return (
+        <>
+        {!connected ? (
+                <section id="bottom-left">
+                    <div className="logo">
+                        <img src={mainLogo} className = "logoImage" alt="HoneyWasp logo" />
+                        <span className="repoinfo">TruFoox/HoneyWasp WebUI {webUIVersion}</span>
+                    </div>
+                </section>
+            ) : (
+                <section id="bottom-left">
+                    <div className="logo">
+                        <img src={mainLogo} className = "logoImage" alt="HoneyWasp logo" />
+                        <div className="repoText">
+                            <span className="repoinfo">TruFoox/HoneyWasp WebUI {webUIVersion}</span>
+                            <br />
+                            <span className="repoinfo">TruFoox/HoneyWasp {honeyWaspVersion} connected ✓</span>
+                        </div>
+                    </div>
+                </section>
+
+            )}
+        </>
+    )
 }
 
 export default App
