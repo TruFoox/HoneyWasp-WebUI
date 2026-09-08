b3a277eed6be56575f0fda091a3ebade7fca8982
Landen Laflamme <landenlaflamme@gmail.com>
2026-09-08 14:50:51 -0700

    Init

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import mainLogo from './assets/Logo.svg'
+
+import './App.css'
+function App() {
+
+    /* Ignore errors on IntelliJ, boy is it stupid. */
+  return (
+    <>
+    <section id="bottom-left">
+      <div className="logo">
+        <img src={mainLogo} className = "logoImage" alt="HoneyWasp logo" />
+        <repoinfo>TruFoox/HoneyWasp WebUI v1.0</repoinfo>
+      </div>
+    </section>
+    <section id="center">
+        <div>
+            <h1>Hi</h1>
+            <p>
+               Hi <code>nerd</code>
+            </p>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default App
