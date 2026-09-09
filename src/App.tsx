2fa2ccb22db661dfcac6dc5b7bedb695f1c120c2
Landen Laflamme <landenlaflamme@gmail.com>
2026-09-08 19:27:48 -0700

    Trying to fix it not connecting to websocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+
 import mainLogo from './assets/Logo.svg'
+import loadingImage from "./assets/loading.gif";
+
+
 import './App.css'
 function App() {
     // Apparently js variables need a setter defined alongside it for it to change dynamically w/ react
@@ -7,35 +11,44 @@ function App() {
     const webUIVersion = "v1.0"
     const [honeyWaspVersion, setHoneyWaspVersion] = useState<string>("Not Fetched")
 
-    let socket: WebSocket // equal to WebSocket socket;
+    const socketRef = useRef(null) // Use socketRef.current when referencing
 
     useEffect(() => { // "Run this when something happens"
-        socket = new WebSocket('ws://localhost:8080')
+        const connect = () => {
+            const socket = new WebSocket('ws://localhost:8080')
+            socketRef.current = socket
 
-        socket.onopen = () => {
-            console.log('Connected to HoneyWasp server')
-            socket.send('WebUI ready')
-        }
+            socket.onopen = () => {
+                console.log('Connected to HoneyWasp server')
+                socket.send('WebUI ready')
+            }
 
-        socket.onmessage = (event) => {
-            console.log('Server:', event.data)
+            socket.onmessage = (event) => {
+                console.log('Server:', event.data)
 
-            if (event.data === 'Acknowledged') {
-                setConnected(true)
-            } else {
-                setHoneyWaspVersion(event.data)
+                if (event.data === 'connected') {
+                    setConnected(true)
+                } else {
+                    setHoneyWaspVersion(event.data)
+                }
+            }
+
+            socket.onclose = () => {
+                setTimeout(connect, 1000)
             }
         }
 
+        connect()
+
         return () => {
-            socket.close()
+            socketRef.current?.close()
         }
     }, []) // [] means on startup after website renders
 
     useEffect(() => {
         if (connected) {
 
-            socket.send(honeyWaspVersion)
+            socketRef.current.send(honeyWaspVersion)
             setHoneyWaspVersion(honeyWaspVersion);
         }
     }, [connected]) // "Send once connected is true"
@@ -43,25 +56,33 @@ function App() {
     return (
         <>
         {!connected ? (
+            <>
                 <section id="bottom-left">
-                    <div className="logo">
-                        <img src={mainLogo} className = "logoImage" alt="HoneyWasp logo" />
+                    <div className="sysinfo">
+                        <img src={mainLogo} className="logoImage" alt="HoneyWasp logo"/>
                         <span className="repoinfo">TruFoox/HoneyWasp WebUI {webUIVersion}</span>
                     </div>
                 </section>
-            ) : (
-                <section id="bottom-left">
-                    <div className="logo">
-                        <img src={mainLogo} className = "logoImage" alt="HoneyWasp logo" />
-                        <div className="repoText">
-                            <span className="repoinfo">TruFoox/HoneyWasp WebUI {webUIVersion}</span>
-                            <br />
-                            <span className="repoinfo">TruFoox/HoneyWasp {honeyWaspVersion} connected ✓</span>
-                        </div>
+                <section id="center">
+                    <div className="loadingScreen">
+                        <img src={loadingImage}/>
+                        <div className="repoText"></div>
                     </div>
                 </section>
+            </>
+        ) : (
+            <section id="bottom-left">
+                <div className="sysinfo">
+                    <img src={mainLogo} className = "logoImage" alt="HoneyWasp logo" />
+                    <div className="repoText">
+                        <span className="repoinfo">TruFoox/HoneyWasp WebUI {webUIVersion}</span>
+                        <br />
+                        <span className="repoinfo">TruFoox/HoneyWasp {honeyWaspVersion} connected ✓</span>
+                    </div>
+                </div>
+            </section>
 
-            )}
+        )}
         </>
     )
 }
