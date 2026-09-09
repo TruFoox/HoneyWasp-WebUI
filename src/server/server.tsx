88652ef60824599610fbdf2f074de8a341eea075
Landen Laflamme <landenlaflamme@gmail.com>
2026-09-08 18:15:00 -0700

    Fixes

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -16,13 +16,11 @@ const wss = new WebSocketServer({ server: httpServer })
 wss.on('connection', function connection(ws) {
     ws.on('error', console.error)
 
-    ws.on('message', function message(data, isBinary) {
+    ws.on('message', function message(data) {
         wss.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
-                client.send(data, { binary: isBinary })
+                client.send(data)
             }
-
-            console.log('Received Message: %s', data)
         })
 
         ws.send('WebUI running')
